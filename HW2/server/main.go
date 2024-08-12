ce8173cf614bbff88292bc17142ad5cb4c2daae3
mivihan <mvgliatser@edu.hse.ru>
2024-08-12 16:07:41 +0300

    Update main.go

--- HW2/server/main.go
+++ HW2/server/main.go
@@ -4,9 +4,15 @@ import (
 	"awesomeProject/accounts"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/google/uuid"
+	"go_minicourse/HW2/server/account"
 )
 
+var secretKey = uuid.New().String()
+
 func main() {
+	println("Секретный ключ:", secretKey)
+	
 	accountsHandler := accounts.New()
 
 	// Echo instance
@@ -16,9 +22,14 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/account", accountsHandler.GetAccount)
-	e.POST("/account/create", accountsHandler.CreateAccount)
+	e.POST("/api/account/create", accountsHandler.CreateAccount)
+	e.GET("/api/account", accountsHandler.GetAccount)
+	e.DELETE("/api/account", accountsHandler.DeleteAccount)
+	e.PATCH("/api/account", accountsHandler.PatchAccount)
+	e.POST("/api/account/rename", accountsHandler.ChangeAccount)
 
+	e.GET("/api/accounts", accountsHandler.GetAll)
+	e.GET("/api/actuator", accountsHandler.Actuator)
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
 }
